13e1d5e474fe270caa9cd0bd7174124b3e31610c
sonquer <sonquer@o2.pl>
2020-10-11 18:50:36 +0200

    Added project with docker support

---
+++ src/Services/ExchangeThings.WebApp/Controllers/ExchangesController.cs
@@ -0,0 +1,20 @@
+﻿using ExchangeThings.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeThings.WebApp.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post(ExchangeModel exchangeModel)
+        {
+            return View("Index", exchangeModel);
+        }
+    }
+}
---
+++ src/Services/ExchangeThings.WebApp/Models/ExchangeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeThings.WebApp.Models
+{
+    public class ExchangeModel
+    {
+        [Required(ErrorMessage = "Name is empty")]
+        public string Name { get; set; }
+    }
+}
